80ee5f1eddb646535b4612902fcf3162bea19da7
amos.ahindukha <amos@bposeas.com>
2023-08-21 11:13:44 +0300

    make message text area

--- portfolio/models.py
+++ portfolio/models.py
@@ -41,7 +41,7 @@ class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     subject = models.CharField(max_length=100)
-    message = models.CharField(max_length=250)
+    message = models.TextField(max_length=250)
 
     def __str__(self):
         return self.name
